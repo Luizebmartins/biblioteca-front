917a296bacfaed50b21d535bf07d0f7b93509237
Luiz Martins <luizmartins.prog@gmail.com>
2022-07-26 19:02:00 -0300

    att diretório

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = "biblioteca-front";
-const app = express();
 
+const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/biblioteca-front'));
@@ -11,11 +10,27 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/biblioteca-front/'}),
 );
 
-// app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-// app.get('/*', (req, res) => {
-// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-// });
-
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// const express = require('express');
+// const path = require('path');
+// const nomeApp = "biblioteca-front";
+// const app = express();
+
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/biblioteca-front'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/biblioteca-front/'}),
+// );
+
+// // app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+// // app.get('/*', (req, res) => {
+// // res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+// // });
+
+
+// app.listen(process.env.PORT || 8080);
