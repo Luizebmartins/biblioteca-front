fda50fbfda06b0d0d87d690958ac3788c3f5a4ae
Luiz Martins <luizmartins.prog@gmail.com>
2022-07-25 23:01:43 -0300

    att - porta para conexão no railway

--- server.js
+++ server.js
@@ -9,4 +9,13 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+
+//app.listen(process.env.PORT || 8080);
+
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+    console.log("Server started.......");
+})
