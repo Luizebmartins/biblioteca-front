5304a6794c998023f6f5ae3375d1da1fb3ca018c
Luiz Martins <luizmartins.prog@gmail.com>
2022-07-26 18:54:07 -0300

    att diretório

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const path = require('path');
 const nomeApp = "biblioteca-front";
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/biblioteca-front'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/biblioteca-front/'}),
+);
+
+// app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+// app.get('/*', (req, res) => {
+// res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+// });
 
 
 app.listen(process.env.PORT || 8080);
