dfdd4f40af9b16fe85cfc2af9f10565860d40b09
Luiz Martins <luizmartins.prog@gmail.com>
2022-07-26 18:47:57 -0300

    att diretório

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = "Biblioteca";
+const nomeApp = "biblioteca-front";
 const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
@@ -10,12 +10,4 @@ res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 
-//app.listen(process.env.PORT || 8080);
-
-
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-app.listen(port, host, function() {
-    console.log("Server started.......");
-})
+app.listen(process.env.PORT || 8080);
